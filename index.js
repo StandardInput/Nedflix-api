ff476cd4c5bc5844cc3f4d76804a16e91698f08a
Robert-Jan Keizer <robertjankeizer@gmail.com>
2015-08-01 12:34:22 +0200

    Making rtl work

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var RTL_BASE_API = "http://rtl.nl/system/s4m/vfd/version=2/fmt=progressive/outpu
 var RTL_FETCH_PATH = "";
 
 
-exports.getRTL = function(type) {
+exports.getRTL = function(type, callback) {
     switch(type) {
         case "catchup": 
             RTL_FETCH_PATH = "/d=a3t/fun=catchup"; 
@@ -20,16 +20,23 @@ exports.getRTL = function(type) {
     }
     return request(RTL_BASE_API + RTL_FETCH_PATH, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        // console.log(body) // Show the HTML for the Google homepage.
-        for (var i = 0; i < 10; i++) {
-            episode.title = i;
-            episode.smallImage = i;
-            episode.largeImage = i;
-            episode.description = i;
-            episode.videoUrl = "http://" + i + ".nl/";
-            dataArr.push(episode);
+        json = JSON.parse(response.body);
+        PROGLOGO_BASE_URL = json.meta.proglogo_base_url;
+        POSTER_BASE_URL = json.meta.poster_base_url;
+
+        for (var i = 0; i < json.abstracts.length; i++) {
+            abstracts = json.abstracts[i];
+            
+            abstractsArr = {}
+            abstractsArr.id = abstracts.key;
+            abstractsArr.name = abstracts.name;
+            abstractsArr.description = abstracts.synopsis;
+            abstractsArr.proglogo = PROGLOGO_BASE_URL + abstracts.proglogo;
+            abstractsArr.poster = POSTER_BASE_URL + abstracts.coverurl;
+            
+            dataArr.push(abstractsArr);
         };
-        JSON.stringify(dataArr, null, 4);
+        callback(JSON.stringify(dataArr, null, 4));
       }
     });
 
