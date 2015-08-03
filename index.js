1ab42aa23cb3dec59829c3bf61d1b78599c2e1a5
Robert-Jan Keizer <robertjankeizer@gmail.com>
2015-08-03 19:26:35 +0200

    Added npo

--- index.js
+++ index.js
@@ -5,10 +5,40 @@ dataArr = [];
 /*
 * Api paths
 */
+var NPO_BASE_API = "http://apps-api.uitzendinggemist.nl"
 var RTL_BASE_API = "http://rtl.nl/system/s4m/vfd/version=2/fmt=progressive/output=json";
 var RTL_FETCH_PATH = "";
 
 
+/*
+* NPO:
+*/
+exports.getNPO = function(callback) {
+    return request(NPO_BASE_API + "/series.json", function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        json = JSON.parse(response.body);
+
+        for (var i = 0; i < json.length; i++) {
+            npo = json[i];
+            
+            npoArr = {}
+            npoArr.id = npo.nebo_id;
+            npoArr.name = npo.name;
+            npoArr.description = npo.description;
+            npoArr.proglogo = npo.image;
+            npoArr.poster = npo.image;
+            
+            dataArr.push(npoArr);
+        };
+        callback(JSON.stringify(dataArr, null, 4));
+      }
+    });
+}
+
+
+/*
+* RTL XL:
+*/
 exports.getRTL = function(type, callback) {
     switch(type) {
         case "catchup": 
@@ -27,14 +57,14 @@ exports.getRTL = function(type, callback) {
         for (var i = 0; i < json.abstracts.length; i++) {
             abstracts = json.abstracts[i];
             
-            abstractsArr = {}
-            abstractsArr.id = abstracts.key;
-            abstractsArr.name = abstracts.name;
-            abstractsArr.description = abstracts.synopsis;
-            abstractsArr.proglogo = PROGLOGO_BASE_URL + abstracts.proglogo;
-            abstractsArr.poster = POSTER_BASE_URL + abstracts.coverurl;
+            rtlArr = {}
+            rtlArr.id = abstracts.key;
+            rtlArr.name = abstracts.name;
+            rtlArr.description = abstracts.synopsis;
+            rtlArr.proglogo = PROGLOGO_BASE_URL + abstracts.proglogo;
+            rtlArr.poster = POSTER_BASE_URL + abstracts.coverurl;
             
-            dataArr.push(abstractsArr);
+            dataArr.push(rtlArr);
         };
         callback(JSON.stringify(dataArr, null, 4));
       }
@@ -68,3 +98,5 @@ exports.getRTLEpisodes = function(id, callback) {
 exports.getRTLVideo = function(id) {
 
 }
+
+
